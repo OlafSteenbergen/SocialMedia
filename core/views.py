3bc38f1556e01627b0f86a69eeef1638f831bcfc
Olaf Steenbergen <steenbergenolaf@gmail.com>
2023-11-04 11:44:29 +0100

    Deploy Railway 1

--- core/views.py
+++ core/views.py
@@ -58,9 +58,8 @@ def index(request):
     return render(request, 'index.html', {'user_profile': user_profile, 'suggestions_username_profile_list': suggestions_username_profile_list[:4], 'posts':posts.order_by('-created_at')})
 
 def search(request, pk):
-    posts = Post.objects.filter(user=pk)
-    return render(request, 'search.html', {'posts':posts})
-
+    posts = Post.objects.filter(caption__icontains=pk)
+    return render(request, 'search.html', {'posts':posts, 'pk':pk})
 
 @login_required(login_url='signin')
 def upload(request):
@@ -76,28 +75,6 @@ def upload(request):
         return redirect('/')
     else:
         return redirect('/')
-
-@login_required(login_url='signin')
-def search(request):
-    user_object = User.objects.get(username=request.user.username)
-    user_profile = Profile.objects.get(user=user_object)
-
-    if request.method == 'POST':
-        username = request.POST['username']
-        username_object = User.objects.filter(username__icontains=username)
-
-        username_profile = []
-        username_profile_list = []
-
-        for users in username_object:
-            username_profile.append(users.id)
-
-        for ids in username_profile:
-            profile_lists = Profile.objects.filter(id_user=ids)
-            username_profile_list.append(profile_lists)
-        
-        username_profile_list = list(chain(*username_profile_list))
-    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
 
 @login_required(login_url='signin')
 def likepost(request):
