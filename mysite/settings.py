39872417c204f3a2379d96cc1d1007c326c9d0b4
Olaf Steenbergen <steenbergenolaf@gmail.com>
2023-11-20 08:46:55 +0100

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-y6on4&^y1+^c$5_h4eh-i2fz-c6jmagos9$5b!o!%8=08s#!tm'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
